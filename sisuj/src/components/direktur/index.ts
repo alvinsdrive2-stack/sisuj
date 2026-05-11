5d6dbce2393a4fc24ea5d864e6be678ba6e14e80
alvinsdrive2-stack <alvins.drive2@gmail.com>
2026-05-11 11:37:33 +0700

    remove sisuj and other projects from tracking

--- sisuj/src/components/direktur/index.ts
+++
@@ -1,4 +0,0 @@
-export { StatCard } from './StatCard'
-export { DocumentCard } from './DocumentCard'
-export { EmptyState } from './EmptyState'
-export { DokumenViewerModal } from './DokumenViewerModal'
