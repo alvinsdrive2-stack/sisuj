859987b1227256f35c5071c3e3e0e324451cdad2
alvinsdrive2-stack <alvins.drive2@gmail.com>
2026-06-02 17:55:48 +0700

    postrest

---
+++ dist/assets/useKegiatanByRole-DNIEMgwk.js
@@ -0,0 +1,1 @@
+import{t,v as r}from"./index-C_JUWd3b.js";import{u as g}from"./asesi-shared-BH0vlEg6.js";function L(){const{user:a}=g(),s=a?.role?.name?.toLowerCase()==="asesor",{kegiatan:e,isLoading:i}=t(!s),{kegiatan:o,isLoading:n}=r(s);return{kegiatan:s?o:e,isLoading:s?n:i,isAsesor:s}}export{L as u};
--- dist/assets/useKegiatanByRole-t_awNxeG.js
+++
@@ -1,1 +0,0 @@
-import{t,v as r}from"./index-BTBATE2_.js";import{u as g}from"./asesi-shared-BH0vlEg6.js";function L(){const{user:a}=g(),s=a?.role?.name?.toLowerCase()==="asesor",{kegiatan:e,isLoading:i}=t(!s),{kegiatan:o,isLoading:n}=r(s);return{kegiatan:s?o:e,isLoading:s?n:i,isAsesor:s}}export{L as u};
